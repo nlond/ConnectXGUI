1876508724c1ed1bf8b0dcabbbc03e68d9209abb
nlond <nlondhe@clemson.edu>
2023-12-24 11:52:14 -0500

    added text and buttons to play again, still need functionality

--- src/cpsc2150/extendedConnectX/views/PlayAgainView.java
+++ src/cpsc2150/extendedConnectX/views/PlayAgainView.java
@@ -20,18 +20,11 @@ public class PlayAgainView extends JFrame implements ActionListener {
         JPanel bottomPanel = new JPanel();
         JPanel centerPanel = new JPanel();
 
-        // set each panel to a different color
-        topPanel.setBackground(Color.RED);
-        leftPanel.setBackground(Color.GREEN);
-        rightPanel.setBackground(Color.YELLOW);
-        bottomPanel.setBackground(Color.MAGENTA);
-        centerPanel.setBackground(Color.BLUE);
-
         // set size of panels
-        topPanel.setPreferredSize(new Dimension(100, 50));
+        topPanel.setPreferredSize(new Dimension(100, 100));
         leftPanel.setPreferredSize(new Dimension(50, 100));
         rightPanel.setPreferredSize(new Dimension(50, 100));
-        bottomPanel.setPreferredSize(new Dimension(100, 50));
+        bottomPanel.setPreferredSize(new Dimension(100, 100));
         centerPanel.setPreferredSize(new Dimension(100, 100));
 
         this.add(topPanel, BorderLayout.NORTH); // attaches panel to the top of this
@@ -40,12 +33,53 @@ public class PlayAgainView extends JFrame implements ActionListener {
         this.add(bottomPanel, BorderLayout.SOUTH); // attaches to bottom of this
         this.add(centerPanel, BorderLayout.CENTER); // attaches to the center of frame
 
-        // new panel inside of center panel -----------------------------------
+        // ------------------- text inside center panel ------------------------------------
 
         centerPanel.setLayout(new BorderLayout()); // makes the center panel have a layout
 
         JPanel centerTopPanel = new JPanel(); // create a new panel
-        centerTopPanel.setBackground(Color.black);
+
+        centerTopPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(0, 0, 10, 0); // Add some space between components
+
+        JPanel centerMidPanel = new JPanel();
+        JPanel centerLowPanel = new JPanel();
+
+        centerTopPanel.setPreferredSize(new Dimension(100, 100));
+        centerMidPanel.setPreferredSize(new Dimension(100, 5));
+        centerLowPanel.setPreferredSize(new Dimension(100, 100));
+
+        centerTopPanel.setBackground(Color.red);
+        centerMidPanel.setBackground(Color.lightGray);
+
+        centerPanel.add(centerTopPanel, BorderLayout.NORTH);
+        centerPanel.add(centerMidPanel, BorderLayout.CENTER);
+        centerPanel.add(centerLowPanel, BorderLayout.SOUTH);
+
+        JLabel label = new JLabel("Player " + whoWon + " Has Won!");
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+        centerTopPanel.add(label, gbc);
+
+        gbc.gridy = 1;
+        JLabel label2 = new JLabel("Do you want to play again?");
+        label2.setFont(new Font("Arial", Font.BOLD, 20));
+        centerTopPanel.add(label2, gbc);
+
+        // -------------------- yes and no buttons ---------------------------
+        centerMidPanel.setLayout(new FlowLayout());
+
+        JButton yesBtn = new JButton("Yes");
+        JButton noBtn = new JButton("No");
+
+        // button settings
+
+
+        centerMidPanel.add(yesBtn);
+        centerMidPanel.add(Box.createHorizontalStrut(10));
+        centerMidPanel.add(noBtn);
 
         //frame settings
         this.setSize(new Dimension(500, 500));
